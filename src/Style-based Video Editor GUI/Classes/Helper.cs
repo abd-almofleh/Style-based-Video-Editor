b332afbce210982e608d9e71997e33e96060faa2
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2021-11-02 01:45:37 +0200

    Adding the VLC libraries to play videos

---
+++ src/Style-based Video Editor GUI/Classes/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Style_based_Video_Editor_GUI.Classes
+{
+    static class Helper
+    {
+       public static  void VlcLibDirectoryNeeded(object sender, Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs e)
+        {
+            e.VlcLibDirectory = new System.IO.DirectoryInfo(System.IO.Path.Combine(".", "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+        }
+    }
+}
