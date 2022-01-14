d6630296207e704d2e04a031b56758b27044ad86
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-01-14 05:16:53 +0200

    Chnaging the settings of the server

--- src/server/app.py
+++ src/server/app.py
@@ -1,10 +1,12 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # noqa
+import warnings  # noqa
+warnings.filterwarnings("ignore")  # noqa
 from flask import Flask, app
 from flask_restful import Api
 from api.routes import create_routes
-import warnings
-warnings.filterwarnings("ignore")
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# -------------------------------
 
 
 def get_flask_app() -> app.Flask:
