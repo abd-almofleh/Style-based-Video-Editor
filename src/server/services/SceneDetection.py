3939fe6af97ba1fda8b6b6866d5cdab0f510d0d9
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-01-28 10:23:51 +0200

    Using threads for cuting the scene and image generation Delete the speech to text code to detect each scene individually

--- src/server/services/SceneDetection.py
+++ src/server/services/SceneDetection.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from .SpeakerDiarisation import SpeakerDiarisation
 from helpers.helper import cut_video, get_frame_form_video, extract_wav_from_video
 from .SpeechToText import SpeechToText
+from threading import Thread, Lock
+_lock = Lock()
 
 
 class SceneDetection:
@@ -9,36 +11,32 @@ class SceneDetection:
     def __init__(self) -> None:
         pass
 
+    @staticmethod
     def extract_scenes(videos_paths):
         scenes_times = SpeakerDiarisation.speaker_change_detection(
             videos_paths[0])
         scenes = dict()
+        threads = []
+
         for i, video_path in enumerate(videos_paths):
             video = Path(video_path)
             video_name = video.stem
-            scenes[video_name] = []
+            scenes[video_name] = [None] * len(scenes_times)
             for i, time in enumerate(scenes_times):
-                scene_info = dict(time)
-                scene_path = cut_video(
-                    video_path, time["start_time"], time["end_time"])
-                scene_info["image"] = get_frame_form_video(
-                    scene_path, time["length"]/2)
-                scene_info["path"] = str(scene_path.absolute().resolve())
-                scenes[video_name].append(scene_info)
-        scenes["scripts"] = []
-        video = Path(videos_paths[0])
-        video_name = video.stem
-        video_scenes = scenes[video_name]
-        recognizer = SpeechToText()
-
-        for scene in video_scenes:
-            if scenes["scripts"] == -1:
-                scenes["scripts"].append({
-                    "arabic_text": "",
-                    "confidence": float(1)
-                })
-                continue
-
-            res = recognizer.arabic_speech_recognition(scene["path"])
-            scenes["scripts"].append(res)
+                x = Thread(target=SceneDetection.process_scene,
+                           args=(i, scenes, time, video_name, video_path), daemon=True)
+                threads.append(x)
+                x.start()
+        for thread in threads:
+            thread.join()
         return scenes
+
+    @staticmethod
+    def process_scene(scene_num, scenes, time, video_name, video_path):
+        scene_info = dict(time)
+        scene_path = cut_video(
+            video_path, time["start_time"], time["end_time"])
+        scene_info["image"] = get_frame_form_video(
+            scene_path, time["length"]/2)
+        scene_info["path"] = str(scene_path.absolute().resolve())
+        scenes[video_name][scene_num] = scene_info
