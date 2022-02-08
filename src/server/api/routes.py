082c2a08d4ef05ac78757322013efca72dda0959
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-02-08 16:39:19 +0200

    Fixing the server config

--- src/server/api/routes.py
+++ src/server/api/routes.py
@@ -4,6 +4,6 @@ from api.generateScenes import GenerateScenes
 
 
 def create_routes(api):
-    api.add_resource(ObjectDetectionApi, '/object-detection')
-    api.add_resource(FaceDetectionApi, '/face-detection')
-    api.add_resource(GenerateScenes, '/generate-scenes')
+  api.add_resource(ObjectDetectionApi, '/object-detection')
+  api.add_resource(FaceDetectionApi, '/face-detection')
+  api.add_resource(GenerateScenes, '/generate-scenes')
--- src/server/app.py
+++ src/server/app.py
@@ -3,23 +3,32 @@ import warnings  # noqa
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # noqa
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # noqa
 warnings.filterwarnings("ignore")  # noqa
-from flask import Flask, app
+from flask import Flask, app, request
 from flask_restful import Api
 from api.routes import create_routes
+from dotenv import load_dotenv
+from pathlib import Path
 
 # -------------------------------
+dotenv_path = '.env'  # Path to .env file
+load_dotenv(dotenv_path)
 
 
 def get_flask_app() -> app.Flask:
 
-    flask_app = Flask(__name__)
-    flask_app.config['MAX_CONTENT_PATH'] = "20000000"
-    api = Api(app=flask_app)
+  flask_app = Flask(__name__)
+  flask_app.config['MAX_CONTENT_PATH'] = "20000000"
+  api = Api(app=flask_app)
 
-    create_routes(api=api)
-    return flask_app
+  create_routes(api=api)
+  return flask_app
+
+
+def before_request():
+  app.logger.info(f"\"{request.method} {request.path}\"")
 
 
 if __name__ == '__main__':
-    app = get_flask_app()
-    app.run(debug=True)
+  app = get_flask_app()
+  app.before_request(before_request)
+  app.run(debug=True, threaded=True)
