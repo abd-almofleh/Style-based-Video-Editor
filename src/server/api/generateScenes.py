b749a32569660f7e0d0608da92e27506b99b49dd
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-02-09 05:07:57 +0200

    Adding dummy data

--- src/server/api/generateScenes.py
+++ src/server/api/generateScenes.py
@@ -2,11 +2,14 @@
 from flask import jsonify, request
 from flask_restful import Resource
 from services.SceneDetection import SceneDetection
+import json
 
 
 class GenerateScenes(Resource):
 
-    def get(self):
-        paths = request.args.getlist("paths")
-        scenes = SceneDetection.extract_scenes(paths)
-        return jsonify({'result': scenes})
+  def get(self):
+    paths = request.args.getlist("paths")
+    # scenes = SceneDetection.extract_scenes(paths)
+    with open('./data/data1.json', encoding='utf-8') as f:
+      scenes = json.load(f)
+    return jsonify({'result': scenes})
